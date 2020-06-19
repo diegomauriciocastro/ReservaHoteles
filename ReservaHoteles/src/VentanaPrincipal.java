cf0ae7456765b95513c1e887d1349032c7e1276e
Diego <diego.mcastro4@gmail.com>
2020-06-19 00:21:11 -0500

    castro commit

--- ReservaHoteles/src/VentanaPrincipal.java
+++ ReservaHoteles/src/VentanaPrincipal.java
@@ -134,7 +134,7 @@ public class VentanaPrincipal extends javax.swing.JFrame {
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new VentanaPrincipal().setVisible(true);
-                mija guaichaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa 
+                mija guaichaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa perreando sola
                 
                         
             }
